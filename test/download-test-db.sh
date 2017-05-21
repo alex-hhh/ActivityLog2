70dfe00cce5ec8e649869b9c0803008d3957f16b
Alex Harsanyi <AlexHarsanyi@gmail.com>
2017-05-21 20:35:25 +0800

    Adjust path

--- test/download-test-db.sh
+++ test/download-test-db.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# NOTE: this script runs from the root of the repository.  This needs to be
+# accounted for in the file paths
+
 set -e
 file_id=0B5h4XOdkim72cDJ4Z2RZR05UOFE
 file_name=test-db.tar.gz.enc
@@ -10,5 +14,6 @@ curl -LJ -o ./$file_name -b ./gcookies "$gurl?export=download&confirm=$code&id=$
 rm ./gcookies
 openssl aes-256-cbc -d -k $TESTDBPW -in $file_name -out $file_name2
 rm $file_name
-tar xvzf $file_name2
+# We run in the root of the repo, but output test databases into the test folder
+tar xvzf $file_name2 -C test
 rm $file_name2
