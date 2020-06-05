alex-hhh/ActivityLog2

commit 3e3ffa7757831025e67e1a2b90740459cc378dd0

Avoid unnecessary refresh calls in map-widget%
It looks like the paint call for a canvas% comes in before the show
notification, so update the zoom level in the paint method as well, if
delayed-resize-to-fit is set.

Also avoid refresh when the map implementation is notified of a size change --
the refresh is handled by the widget or snip classes.

rkt/widgets/map-widget/map-widget.rkt
@@ -76,6 +76,11 @@
         (let-values ([(w h) (send canvas get-size)])
           (send map-impl resize w h))
         (set! first-paint? #f))
+      (when delayed-resize-to-fit?
+        ;; Handle the delayed resize-to-fit here, it seems that handling it in
+        ;; `on-superwindow-show` is done after the first paint is called.
+        (resize-to-fit)
+        (set! delayed-resize-to-fit? #f))
       (send map-impl draw dc 0 0))
 
     ;; This is the canvas on which we paint the map.  We intercept the mouse

rkt/widgets/map-widget/map-impl.rkt
@@ -4,7 +4,7 @@
 ;; canvas, snip and maybe pasteboard views
 ;;
 ;; This file is part of ActivityLog2 -- https://github.com/alex-hhh/ActivityLog2
-;; Copyright (c) 2019 Alex Harsányi <AlexHarsanyi@gmail.com>
+;; Copyright (c) 2019, 2020 Alex Harsányi <AlexHarsanyi@gmail.com>
 ;;
 ;; This program is free software: you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the Free
@@ -586,7 +586,10 @@
       (limit-origin w h)
       (set! width w)
       (set! height h)
-      (refresh))
+      ;; NOTE: do not call refresh here, we were informed by our "container"
+      ;; (map-widget% or map-snip%) that we were resized, and they are
+      ;; responsible for refreshing themselves if they consider it necessary.
+      )
 
     (define last-mouse-x #f)
     (define last-mouse-y #f)

commit 0edd5fbdea691a419ac53a5fa6585bc0873c26ea

Fix zoom-to-lap functionality in the map widget (AB#7)
Bug introduced in 3e3ffa7757831025e67e1a2b90740459cc378dd0

Updated the map canvas with a new method, `is-visible?` which becomes true
after either `on-paint` or `on-superwindow-show` is called for the first time.
This is used by `resize-to-fit` to know when to delay the resize.

rkt/widgets/map-widget/map-widget.rkt
@@ -72,21 +72,21 @@
     (define (on-canvas-paint canvas dc)
       (when first-paint?
         ;; If this is our first paint, we can determine the size of the canvas
-        ;; (before the first paint invocation, the size will be 0.
+        ;; (before the first paint invocation, the size will be 0).
         (let-values ([(w h) (send canvas get-size)])
           (send map-impl resize w h))
         (set! first-paint? #f))
       (when delayed-resize-to-fit?
         ;; Handle the delayed resize-to-fit here, it seems that handling it in
         ;; `on-superwindow-show` is done after the first paint is called.
-        (resize-to-fit)
-        (set! delayed-resize-to-fit? #f))
+        (resize-to-fit))
       (send map-impl draw dc 0 0))
 
     ;; This is the canvas on which we paint the map.  We intercept the mouse
     ;; and keyboard events to pass them on to the map-impl% object.
     (define canvas
       (new (class canvas% (init) (super-new)
+             (inherit get-dc)
              (define/override (on-size w h)
                (send map-impl resize w h))
              (define/override (on-event event)
@@ -95,12 +95,25 @@
              (define/override (on-char event)
                (define dc (send this get-dc))
                (send map-impl on-char dc 0 0 0 0 event))
+
+             ;; NOTE: the `on-paint` method is called before
+             ;; on-superwindow-show, and the is-shown? method returns true
+             ;; before `on-superwindow-show` is called.  We attempt to control
+             ;; ordering here, so that a resize to fit is called before the
+             ;; first paint is done.  See also AB#7
+             (define visible? #f)
+
              (define/override (on-superwindow-show shown?)
-               (when (and shown? delayed-resize-to-fit?)
-                 (resize-to-fit)
-                 (set! delayed-resize-to-fit? #f))))
+               (unless (equal? visible? shown?)
+                 (set! visible? shown?)
+                 (when (and shown? delayed-resize-to-fit?)
+                   (resize-to-fit))))
+             (define/override (on-paint)
+               (set! visible? #t)
+               (on-canvas-paint this (get-dc)))
+             (define/public (is-visible?)
+               visible?))
            [parent parent]
-           [paint-callback on-canvas-paint]
            [style '(no-autoclear)]))
 
     ;; The map implementation, handles drawing and event handling
@@ -161,9 +174,12 @@
       (send map-impl move-to position))
 
     (define/public (resize-to-fit [group #f])
-      (when (send canvas is-shown?)     ; AB#7
-        (send map-impl resize-to-fit group)
-        (set! delayed-resize-to-fit? #t)))
+      (if (send canvas is-visible?)     ; AB#7
+          (begin
+            (send map-impl resize-to-fit group)
+            (set! delayed-resize-to-fit? #f))
+          (begin
+            (set! delayed-resize-to-fit? #t))))
 
     (define/public (export-image-to-file file-name)
       (send map-impl export-image-to-file file-name))