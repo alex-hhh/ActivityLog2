4ae6bbccfb7c69a88ba4a644c1950e4ea43d21c1
Alex Harsányi <AlexHarsanyi@gmail.com>
2020-11-14 16:07:30 +0800

    Altitude functions which didn't make it
    As part of #51, several improvements were made to the elevation correction
    algorithm.  This commit contains the smoothing algorithms which did not make
    it into the final version:
    
    * `smooth-altitude/friedl`: is the smoothing algorithm used previously.  It is
    a variation of RDP simplification
    
    * `smooth-altitude/rdp`: is a RDP based simplification algorithm
    
    * `instant-elevation-change`: is an algorithm used to determine how much the
    elevation changes from one point to the next -- it can be used to calculate
    epsilon values for the two smooth-altitude algorithms, instead of using fixed
    numbers, since it appears that fixed values are not suitable for all
    activities and terrain types.

--- rkt/models/elevation-correction.rkt
+++ rkt/models/elevation-correction.rkt
@@ -279,6 +279,194 @@
 
 ;;...................................................... smooth-altitude ....
 
+;; Smooth the altitude points produced by 'average-altitude' using an idea
+;; from http://regex.info/blog/2015-05-09/2568, the resulting altitude is only
+;; marginally better from a visual point of view (and somewhat worse for small
+;; elevation changes), but total ascent and descent calculated off this
+;; smoothed data are significantly more accurate.
+;;
+;; TRACKPOINTS is a vector of TPOINT structures and each point will have their
+;; CALT slot updated in place.
+;;
+;; MINIMUM-DISTANCE is the smallest distance between which we can smooth the
+;; altitude.  For distances less than this, we interpolate between the start
+;; and end point, for larger distances, we split the range
+;;
+;; MINIMUM-ALTITUDE is the minimum altidute difference in a range for which we
+;; split the range.  If the altitude difference in a range is less than this,
+;; we consider the range monotonic.
+(define (smooth-altitude/friedl
+         trackpoints
+         #:minimum-distance [minimum-distance 50.0]
+         #:minimum-altitude [minimum-altitude 3.0])
+
+  ;; Compute the distance between START and END (indexes in the TRACKPOINTS
+  ;; vector)
+  (define (delta-dist start end)
+    (- (tpoint-dst (vector-ref trackpoints end))
+       (tpoint-dst (vector-ref trackpoints start))))
+  ;; Compute the altitude change between START and END (indexes in the
+  ;; TRACKPOINTS vector).  This will be negative if the slope is downhill.
+  (define (delta-alt start end)
+    (- (tpoint-calt (vector-ref trackpoints end))
+       (tpoint-calt (vector-ref trackpoints start))))
+  ;; Fill in a monotonic altitude increase/decrease between START and END.
+  (define (monotonic-slope start end)
+    (let* ((delta-dst (delta-dist start end))
+           (delta-alt (delta-alt start end))
+           (start-dst (tpoint-dst (vector-ref trackpoints start)))
+           (start-alt (tpoint-calt (vector-ref trackpoints start))))
+      (for ([idx (in-range start (add1 end))])
+        (let* ((dst (tpoint-dst (vector-ref trackpoints idx)))
+               (nalt
+                (if (> delta-dst 0)     ; the device can be stationary!
+                    (+ start-alt (* delta-alt (/ (- dst start-dst) delta-dst)))
+                    start-alt)))
+          (set-tpoint-calt! (vector-ref trackpoints idx) nalt)))))
+  ;; Find the minimum and maximum altitude between START and END and return 4
+  ;; values: min-alt, min-alt position, max-alt, max-alt position.
+  (define (find-min-max-alt start end)
+    (let ((min-alt (tpoint-calt (vector-ref trackpoints start)))
+          (min-alt-idx start)
+          (max-alt (tpoint-calt (vector-ref trackpoints start)))
+          (max-alt-idx start))
+      (for ([idx (in-range start (add1 end))])
+        (define a (tpoint-calt (vector-ref trackpoints idx)))
+        (when (< a min-alt)
+          (set! min-alt a)
+          (set! min-alt-idx idx))
+        (when (> a max-alt)
+          (set! max-alt a)
+          (set! max-alt-idx idx)))
+      (values min-alt min-alt-idx max-alt max-alt-idx)))
+  ;; Return the position of the middle point between START and END.  This is
+  ;; done based on distances, not indices, and depending on sampling rates and
+  ;; stop points, it might be "off" from the middle. Still, the "best" middle
+  ;; point is returned.
+  (define (find-middle start end)
+    (let* ((sdist (tpoint-dst (vector-ref trackpoints start)))
+           (half (/ (delta-dist start end) 2))
+           (mid (bsearch trackpoints (+ sdist half)
+                         #:start start #:stop end #:key tpoint-dst)))
+      mid))
+  (define (order-points p1 p2 p3 p4)
+    (let ((points (list p1 p2 p3 p4)))
+      (remove-duplicates (sort points <))))
+
+  ;; Split the interval between START and END between the highest and lowest
+  ;; point, until the interval is too small or the altitude difference is less
+  ;; than MINIMUM-ALTITUDE, in which case the altitude is simply interpolated
+  ;; between start and end.
+  (define (iterate start end)
+    (let ((dist (delta-dist start end)))
+      (cond
+        ((or (<= (- end start) 1) (< dist minimum-distance))
+         (monotonic-slope start end))
+        (#t
+         (let-values (((min-alt min-alt-idx max-alt max-alt-idx)
+                       (find-min-max-alt start end)))
+           (if (< (- max-alt min-alt) minimum-altitude)
+               (monotonic-slope start end)
+               (let ((ranges (order-points start min-alt-idx max-alt-idx end)))
+                 (if (= (length ranges) 2)
+                     ;; range is monotonic, split it in two and recurse
+                     (let ((mid (find-middle start end)))
+                       (iterate start (sub1 mid))
+                       (iterate mid end))
+                     ;; Else, iterate over the defined ranges
+                     (for ([s ranges] [e (cdr ranges)])
+                       (iterate s e))))))))))
+
+  (when (and (> (vector-length trackpoints) 0)
+             ;; At least one valid altitude in the data set
+             (for/first ((tp (in-vector trackpoints)) #:when (tpoint-calt tp)) #t))
+
+    ;; Fixup #f's in the calt values, this works OK for one-off missing
+    ;; values, if whole ranges are missing, this will not produce nice
+    ;; results.
+    (for ([(tp idx) (in-indexed (in-vector trackpoints))] #:unless (tpoint-calt tp))
+      (if (= idx 0)
+          ;; Scan forward for the first good altitude value
+          (let ((a (for/first ([tp (in-vector trackpoints)] #:when (tpoint-calt tp))
+                     (tpoint-calt tp))))
+            (set-tpoint-calt! tp a))
+          ;; Use the previous value
+          (set-tpoint-calt! tp (tpoint-calt (vector-ref trackpoints (sub1 idx))))))
+
+    (iterate 0 (sub1 (vector-length trackpoints)))))
+
+;; An altitude smoothing algorithm which uses RDP simplification.  See remarks
+;; on `smooth-altitude/friedl` for its purpose.
+;;
+;; TRACKPOINTS is a vector of TPOINT structures and each point will have their
+;; CALT slot updated in place.
+;;
+;; EPSILON is the minimum deviation (in meters) for which we would split the
+;; range.
+(define (smooth-altitude/rdp trackpoints #:epsilon [epsilon 3.0])
+  (define idata (for/vector ([p (in-vector trackpoints)]
+                             [i (in-naturals)]
+                             #:when (tpoint-calt p))
+                  (vector (tpoint-dst p) (tpoint-calt p) i)))
+  (define sdata (rdp-simplify idata #:epsilon epsilon #:destroy-original? #t))
+  (when (>= (vector-length sdata) 2)
+    (for ([p1 (in-vector sdata)]
+          [p2 (in-vector sdata 1)])
+      (match-define (vector d1 a1 i1) p1)
+      (match-define (vector d2 a2 i2) p2)
+      (define distance (- d2 d1))
+      (define height (- a2 a1))
+      (for ([i (in-range i1 i2)])
+        (define p (vector-ref trackpoints i))
+        (set-tpoint-calt! p (+ a1 (* height (/ (- (tpoint-dst p) d1) distance))))))))
+
+;; Determine the maximum elevation change between two adjacent points in
+;; TRACKPOINTS, ignoring outliers.  Outliers are points which have a huge
+;; elevation change and they need to be ignored, because skiing activities
+;; will have large elevation changes where the skiing lift was taken and
+;; recording stopped.
+;;
+;; Implementation note: outliers are determined using the following algorithm:
+;; the 25% and 75% quantile are determined and the difference between them is
+;; the inter-quantile range.  Outliers are points which are more than 1.5
+;; times the inter-quantile range away from either the 25% or 75% quantiles.
+;; This is the common method used in R.
+;;
+;; Also note that, instead of using the `quantile` function from the math
+;; package, we roll our own, since using that function would require a lot of
+;; data copying on possibly large vectors.
+(define (instant-elevation-change trackpoints)
+  (define scale 1.5)
+  (if (= 0 (vector-length trackpoints))
+      #f
+      (let ([deltas (make-vector (sub1 (vector-length trackpoints)))])
+        (define last-index
+          (for/fold ([index 0]
+                     [prev-value (tpoint-calt (vector-ref trackpoints 0))]
+                     #:result index)
+                    ([p (in-vector trackpoints 1)])
+            (define this-value (tpoint-calt p))
+            (if (and prev-value this-value)
+                (begin
+                  (vector-set! deltas index (abs (- prev-value this-value)))
+                  (values (add1 index) this-value))
+                (values index this-value))))
+        (if (= last-index 0)
+            #f
+            (let* ([q25-pos (max 0 (exact-ceiling (- (* 0.25 last-index) 1)))]
+                   [q75-pos (max 0 (exact-ceiling (- (* 0.75 last-index) 1)))])
+              (vector-sort! deltas < 0 last-index)
+              (let* ([q25 (vector-ref deltas q25-pos)]
+                     [q75 (vector-ref deltas q75-pos)]
+                     [inter-quantile-range (- q75 q25)]
+                     [upper-outlier-limit (+ q75 (* scale inter-quantile-range))]
+                     #;[lower-outlier-limit (- q25 (* scale inter-quantile-range))])
+                ;; NOTE: deltas are now sorted, so the largest value which is
+                ;; not an outlier can be found by scanning backwards.
+                (for/first ([delta (in-vector deltas (sub1 last-index) 0 -1)]
+                            #:when (<= delta upper-outlier-limit))
+                  delta)))))))
+
 ;; Smooth the altitude in TRACKPOINTS using a low pass filter.  The filter
 ;; width is determined empirically, but it is adjusted to the average distance
 ;; between the points on the track, meaning slower activities (e.g. hiking)
