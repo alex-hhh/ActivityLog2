4b7ef8c99a4f5ae06144d9b5e5f17ad5bfd8bf2c
Alex Harsányi <AlexHarsanyi@gmail.com>
2026-04-17 12:36:18 +0800

    Update build to use Racket 9.1 and update submodules

--- etc/scripts/install-racket.sh
+++ etc/scripts/install-racket.sh
@@ -49,7 +49,7 @@ case "$RACKET_VERSION" in
     6.[0-4] | 6.[0-4].[0-9])
         URL="${BASE}/${V}/racket-${M}${V}-x86_64-linux-ubuntu-precise.sh"
         ;;
-    7.*-cs | 8.*-cs)
+    7.*-cs | 8.*-cs | 9.*-cs)
         # NOTE: 7.4 is the first version which has a Chez variant
         V=`echo $V | sed s/-cs//`
         URL="${BASE}/${V}/racket-${M}${V}-x86_64-linux-cs.sh"
